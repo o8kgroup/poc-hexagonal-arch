c059829e59b1bcc03e9ad33f4c8a8e590088d5a1
Ramon Pacheco <rmpacheco2@gmail.com>
2022-11-26 09:03:58 -0300

    add: add named annotation

--- src/main/kotlin/com/o8k/poc/hexagonalarch/domain/student/usecase/CreateStudentUseCaseImpl.kt
+++ src/main/kotlin/com/o8k/poc/hexagonalarch/domain/student/usecase/CreateStudentUseCaseImpl.kt
@@ -4,7 +4,9 @@ import com.o8k.poc.hexagonalarch.domain.student.Student
 import com.o8k.poc.hexagonalarch.domain.student.exceptions.InvalidStudentException
 import com.o8k.poc.hexagonalarch.domain.student.ports.input.CreateStudentUseCaseInterface
 import com.o8k.poc.hexagonalarch.domain.student.ports.output.StudentPersistenceInterface
+import javax.inject.Named
 
+@Named
 class CreateStudentUseCaseImpl(private val studentData: StudentPersistenceInterface) : CreateStudentUseCaseInterface {
     override fun save(student: Student): Student {
         if (!student.isValid()) {
